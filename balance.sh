df4d6ebed9dfb51bcf952381aa9f1ebfcd793f2b
Mo Lin <molin@wanchain.org>
2019-09-29 12:17:37 +0800

    update some assistant script

--- balance.sh
+++ balance.sh
@@ -5,9 +5,10 @@ BOT_PRIVATE_KEY=0xb89dd4d3e9c0c5a685db7d60e6c7b9805b1bf5b71a9948a4da409e6e5c0ddc
 BOT_BASE_TOKEN=TCA \
 BOT_QUOTE_TOKEN=TCD \
 BOT_BASE_URL=https://testdex.wandevs.org:43001 \
-BOT_MIN_PRICE=0.48 \
-BOT_MAX_PRICE=0.52 \
-BOT_PRICE_GAP=0.003 \
+BOT_MIN_PRICE=0.008 \
+BOT_MAX_PRICE=0.015 \
+BOT_PRICE_GAP=0.1 \
 BOT_EXPAND_INVENTORY=2 \
 BOT_WEB3_URL=https://testdex.wandevs.org:48545 \
-go run main.go
+./bin/amm-balance-price
+
--- cancel.sh
+++ cancel.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
 BOT_TYPE=CONST_PRODUCT \
 BOT_PRIVATE_KEY=0xb89dd4d3e9c0c5a685db7d60e6c7b9805b1bf5b71a9948a4da409e6e5c0ddca4 \
 BOT_BASE_TOKEN=TCA \
 BOT_QUOTE_TOKEN=TCD \
 BOT_BASE_URL=https://testdex.wandevs.org:43001 \
-BOT_MIN_PRICE=0.48 \
-BOT_MAX_PRICE=0.52 \
-BOT_PRICE_GAP=0.003 \
+BOT_MIN_PRICE=0.5 \
+BOT_MAX_PRICE=1.1 \
+BOT_PRICE_GAP=0.03 \
 BOT_EXPAND_INVENTORY=2 \
 BOT_WEB3_URL=https://testdex.wandevs.org:48545 \
-go run main.go
+./bin/amm-cancel
